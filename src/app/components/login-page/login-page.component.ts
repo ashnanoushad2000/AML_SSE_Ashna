259bd2f9585e7bc9203579f3e288e31cc0d33f53
MuhammadIlyas2 <150243304+MuhammadIlyas2@users.noreply.github.com>
2024-11-14 01:54:51 +0000

    Login Page

---
+++ src/app/components/login-page/login-page.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+@Component({
+  selector: 'app-login-page',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './login-page.component.html',
+  styleUrl: './login-page.component.css'
+})
+export class LoginPageComponent {
+  text: string;
+  day: string;
+  onSubmit(){
+    alert('Submit')
+  }
+
+}
