ed78f0de4ffbe85473baa3055690374e7435e1a5
ashnanoushad2000 <126799856+ashnanoushad2000@users.noreply.github.com>
2024-11-15 18:55:19 +0000

    homepage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './components/header/header.component';
 import { LoginPageComponent } from "./components/login-page/login-page.component";
 import { RegistrationPageComponent } from './components/registration-page/registration-page.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, LoginPageComponent, RegistrationPageComponent],
+  imports: [RouterOutlet, LoginPageComponent, RegistrationPageComponent, HeaderComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,14 +3,19 @@ import { Routes, RouterModule} from '@angular/router';
 import { LoginPageComponent } from './components/login-page/login-page.component';
 import { RegistrationPageComponent } from './components/registration-page/registration-page.component';
 import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+
+
 
 export const routes: Routes = [{
     path: '', component: LoginPageComponent},
     { path: 'registration-page', component: RegistrationPageComponent
     },
     {
-        path: 'header', component: HeaderComponent
-    }
+         path: 'header', component: HeaderComponent
+    },
+    { path: 'home', component: HomeComponent },
+//   { path: '**', redirectTo: '' },
 ];
 
 Imports: [LoginPageComponent, RouterModule.forRoot(routes, {enableTracing: true})]
