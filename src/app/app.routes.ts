af3c88acd2c35c57c9e15c9a9832ffb74968eefb
MuhammadIlyas2 <150243304+MuhammadIlyas2@users.noreply.github.com>
2024-11-15 16:30:17 +0000

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,15 @@
 import { Routes, RouterModule} from '@angular/router';
 import { LoginPageComponent } from './components/login-page/login-page.component';
 import { RegistrationPageComponent } from './components/registration-page/registration-page.component';
+import { HeaderComponent } from './components/header/header.component';
 
 export const routes: Routes = [{
     path: '', component: LoginPageComponent},
-    { path: 'registration-page', component: RegistrationPageComponent}
+    { path: 'registration-page', component: RegistrationPageComponent
+    },
+    {
+        path: 'header', component: HeaderComponent
+    }
 ];
 
 Imports: [LoginPageComponent, RouterModule.forRoot(routes, {enableTracing: true})]
