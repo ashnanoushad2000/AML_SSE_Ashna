145cc60863e16035497fd8f98b13d26715c8a05e
MuhammadIlyas2 <150243304+MuhammadIlyas2@users.noreply.github.com>
2024-11-18 00:41:19 +0000

    staff login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { LoginPageComponent } from './components/login-page/login-page.component
 import { RegistrationPageComponent } from './components/registration-page/registration-page.component';
 import { HeaderComponent } from './components/header/header.component';
 import { PaymentsComponent } from './components/payments/payments.component';
+import { StaffLoginComponent } from './components/staff-login/staff-login.component';
 
 export const routes: Routes = [{
     path: '', component: LoginPageComponent},
@@ -11,6 +12,9 @@ export const routes: Routes = [{
     },
     {
         path: 'payments', component: PaymentsComponent
+    },
+    {
+        path: 'staff', component: StaffLoginComponent
     }
 ];
 
